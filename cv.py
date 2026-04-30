f1e76ae9c6838862db88beafa17a2a9e9b54dd8b
dalacoulibaly <dalacoulibaly18@gmail.com>
2026-04-30 19:46:45 +0000

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,41 @@
+import streamlit as cv
+
+cv.write("TECHNICIEN SUPERIEUR EN GEOMATIQUE")
+
+
+cv.warning("Diplômes:")
+
+cv.write("Licence en Géographie Humaine à l'Université Cheikh Anta Diop")
+
+cv.write("BTS en Géomatique au Centre d'Entreprenariat et de Développement Technique")
+
+cv.write("Attestation de Pretation de Service délivrée par l'ANSD (RGPH5)")
+
+cv.write(" baccalauriat à l'Université Cheikh Anta Diop de Dakar ")
+
+
+
+cv.warning("Competences:")
+
+cv.write("Collecte de donnée")
+cv.write("Numerisation")
+cv.write("Topographie")
+cv.write("Conception de plan sur Autocad")
+cv.write("realisation de carte SIG")
+cv.write("python")
+cv.write("cartographie")
+
+
+
+cv.warning("CONNAISSANCE INFORMATIQUE:")
+
+cv.write("Suite Office")
+
+with cv.sidebar:
+
+        cv.write("DALA COULIBALY")
+        cv.write("Mariée et un enfant")
+        cv.write("Mail:dalacoulibaly18@gmal.com")
+        cv.write("Adresse:Dakar")
+        
+        
